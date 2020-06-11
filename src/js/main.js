7cefe7fec86d93de8b73926ef0dd51ca83baa771
Eugene-im <em.92@mail.ru>
2020-06-11 18:17:27 +0300

    add fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-
+    var x = document.getElementsByClassName('dwn');
     var Name = "Unknown OS";
     if (navigator.userAgent.indexOf("Win") != -1) Name =
         "Windows OS";
@@ -13,39 +13,42 @@ function ready() {
         "Android OS";
     if (navigator.userAgent.indexOf("like Mac") != -1) Name =
         "iOS";
+    [].forEach.call(x, function (el) {
+        el.href = '#' + Name;
+        el.innerHTML += ' ' + Name
+    })
     dwnlink.href = '#' + Name;
-    // dwnlink.inneRHTML = '#' + Name;
 }
 
 document.addEventListener('click', function (e) {
-            // console.log(e.target)
-            if (e.target.classList.contains('drop__button')) {
-                document.querySelectorAll('.drop__button + .drop__list')[0].classList.toggle('visible')
-            } else if (document.querySelectorAll('.drop__button + .drop__list')[0].classList.contains('visible')) {
-                document.querySelectorAll('.drop__button + .drop__list')[0].classList.toggle('visible')
-                }
-            })
-        //or
+    // console.log(e.target)
+    if (e.target.classList.contains('drop__button')) {
+        document.querySelectorAll('.drop__button + .drop__list')[0].classList.toggle('visible')
+    } else if (document.querySelectorAll('.drop__button + .drop__list')[0].classList.contains('visible')) {
+        document.querySelectorAll('.drop__button + .drop__list')[0].classList.toggle('visible')
+    }
+})
+//or
 
-        // function getOS() {
-        //     var userAgent = window.navigator.userAgent,
-        //         platform = window.navigator.platform,
-        //         macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-        //         windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-        //         iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-        //         os = null;
+// function getOS() {
+//     var userAgent = window.navigator.userAgent,
+//         platform = window.navigator.platform,
+//         macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+//         windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+//         iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+//         os = null;
 
-        //     if (macosPlatforms.indexOf(platform) !== -1) {
-        //       os = 'Mac OS';
-        //     } else if (iosPlatforms.indexOf(platform) !== -1) {
-        //       os = 'iOS';
-        //     } else if (windowsPlatforms.indexOf(platform) !== -1) {
-        //       os = 'Windows';
-        //     } else if (/Android/.test(userAgent)) {
-        //       os = 'Android';
-        //     } else if (!os && /Linux/.test(platform)) {
-        //       os = 'Linux';
-        //     }
+//     if (macosPlatforms.indexOf(platform) !== -1) {
+//       os = 'Mac OS';
+//     } else if (iosPlatforms.indexOf(platform) !== -1) {
+//       os = 'iOS';
+//     } else if (windowsPlatforms.indexOf(platform) !== -1) {
+//       os = 'Windows';
+//     } else if (/Android/.test(userAgent)) {
+//       os = 'Android';
+//     } else if (!os && /Linux/.test(platform)) {
+//       os = 'Linux';
+//     }
 
-        //     return os;
-        //   }
+//     return os;
+//   }
