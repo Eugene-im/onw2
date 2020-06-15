3ae8fbb0877ed8c0085e79de52b4011de45864e1
Eugene-im <em.92@mail.ru>
2020-06-16 00:00:06 +0300

    add privacy

--- src/js/main.js
+++ src/js/main.js
@@ -53,27 +53,3 @@ function hoverItem(e){
 }
 actionItem1.addEventListener('mouseover', hoverItem);
 actionItem2.addEventListener('mouseover', hoverItem);
-//or
-
-// function getOS() {
-//     var userAgent = window.navigator.userAgent,
-//         platform = window.navigator.platform,
-//         macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-//         windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-//         iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-//         os = null;
-
-//     if (macosPlatforms.indexOf(platform) !== -1) {
-//       os = 'Mac OS';
-//     } else if (iosPlatforms.indexOf(platform) !== -1) {
-//       os = 'iOS';
-//     } else if (windowsPlatforms.indexOf(platform) !== -1) {
-//       os = 'Windows';
-//     } else if (/Android/.test(userAgent)) {
-//       os = 'Android';
-//     } else if (!os && /Linux/.test(platform)) {
-//       os = 'Linux';
-//     }
-
-//     return os;
-//   }
