e447547ed8c174953567c7d72dd6cf5ccf46a8a3
Eugene-im <em.92@mail.ru>
2020-07-13 19:18:23 +0300

    add fixes mob iOS 14,15,16,18,19,20,21,23,25

--- src/js/main.js
+++ src/js/main.js
@@ -8,35 +8,19 @@ var ww = window.innerWidth;
 var elXscrol1 = document.querySelectorAll('.action__item_description__list.mob')[0];
 var elXscrol2 = document.querySelectorAll('.action__item_description__list.mob')[1];
 
-elXscrol1.onscroll = function (e) { changeBackOnscroll(e, 1) }
-elXscrol2.onscroll = function (e) { changeBackOnscroll(e, 2) }
-
-function changeBackOnscroll(e, i) {
-    var elwidth = e.target.firstElementChild.offsetWidth;
-    var counter = Math.floor((e.target.scrollLeft + (ww - elwidth)) / elwidth);
-    var elementsScrolled = document.querySelectorAll('#fixedPhone' + i + ' .item__h1');
-    [].forEach.call(elementsScrolled, function (item, index) {
-        if (index != counter) item.classList.remove('visible');
-        else item.classList.add('visible');
-    });
-}
-
-document.addEventListener("DOMContentLoaded", ready);
 window.onscroll = function () {
     stickyHeader();
     if ((screen.width <= 1023) && (screen.width >= 560)) {
         stickyPhone(actionItem1, 1);
         stickyPhone(actionItem2, 2);
+    };
+    if (document.querySelectorAll('.drop__button + .drop__list_wrap')[0].classList.contains('visible')) {
+        document.querySelectorAll('.drop__button + .drop__list_wrap')[0].classList.toggle('visible')
     }
 };
-playvid.addEventListener('click', function () {
-    document.getElementsByClassName('overflow')[0].classList.toggle('visible')
-})
-document.getElementsByClassName('overflow')[0].addEventListener('click', function (e) {
-    e.stopPropagation();
-    this.classList.toggle('visible')
-})
-document.addEventListener('click', function (e) {
+
+document.addEventListener("DOMContentLoaded", ready);
+document.onclick = function (e) {
     var id = e.target.id;
 
     if (e.target.classList.contains('drop__button')) {
@@ -56,12 +40,42 @@ document.addEventListener('click', function (e) {
             }
         });
     }
-})
+}
 
-actionItem1.onmouseover = hoverItem;
-actionItem1.onmouseout = hoverItem;
-actionItem2.onmouseover = hoverItem;
-actionItem2.onmouseout = hoverItem;
+if ((elXscrol1 !== undefined)&&(elXscrol1 !== undefined)){
+    elXscrol1.onscroll = function (e) { changeBackOnscroll(e, 1) };
+    elXscrol2.onscroll = function (e) { changeBackOnscroll(e, 2) };
+}
+
+if (playvid !== undefined){
+    playvid.onclick = function () {
+        overflow.classList.toggle('visible')
+    };
+}
+
+if (overflow !== undefined){
+    overflow.onclick = function (e) {
+        e.stopPropagation();
+        this.classList.toggle('visible')
+    };
+};
+
+if ((actionItem1 !== undefined)&&(actionItem2 !== undefined)){
+    actionItem1.onmouseover = hoverItem;
+    actionItem1.onmouseout = hoverItem;
+    actionItem2.onmouseover = hoverItem;
+    actionItem2.onmouseout = hoverItem;
+}
+
+function changeBackOnscroll(e, i) {
+    var elwidth = e.target.firstElementChild.offsetWidth;
+    var counter = Math.floor((e.target.scrollLeft + (ww - elwidth)) / elwidth);
+    var elementsScrolled = document.querySelectorAll('#fixedPhone' + i + ' .item__h1');
+    [].forEach.call(elementsScrolled, function (item, index) {
+        if (index != counter) item.classList.remove('visible');
+        else item.classList.add('visible');
+    });
+}
 
 function ready() {
     var x = document.getElementsByClassName('dwn');
